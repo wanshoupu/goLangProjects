073a86a47f9677e55c968fac29a9e28d30706457
Shoupu Wan <shoupu_wan@apple.com>
2019-04-13 10:44:37 -0700

    Format code

--- src/shoupu/cpp/qsort.cpp
+++ src/shoupu/cpp/qsort.cpp
@@ -34,12 +34,12 @@ void rand_swap(int* s, int* e) {
     swap(e - 1, s + 2); // e - 1, has the maximum of the 3
 }
 
-int* part_nested_loop(int* s, int*e) {
+int* part_nested_loop(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
-    while(true) {
-        while(s < e && *++s < *pivot);  // find the next element >= pivot
-        while(pivot < e && *pivot < *--e); // find the prev element <= pivot
+    while (true) {
+        while (s < e && *++s < *pivot); // find the next element >= pivot
+        while (pivot < e && *pivot < *--e); // find the prev element <= pivot
         if (s < e) {
             swap(s, e); //swap the out-of-place elements
         } else {
@@ -52,10 +52,10 @@ int* part_nested_loop(int* s, int*e) {
 int* part_nested_loop_var3(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
-    while(true) {
+    while (true) {
         // '<=' or '<' is not critical. But '<=' brings more saving on unnecessary swaps.
-        while(s < e && *++s <= *pivot);
-        while(pivot < e && *pivot <= *--e);
+        while (s < e && *++s <= *pivot);
+        while (pivot < e && *pivot <= *--e);
         if (s < e) {
             swap(s, e);
         } else {
@@ -66,18 +66,18 @@ int* part_nested_loop_var3(int* s, int* e) {
 }
 
 /**
- * Found another variation of partition scheme based on part_nested_loop
- * Diff wise, it lies between part_nested_loop and part_nested_loop_var
- * - the order of the two while loops is identical to part_nested_loop
- * - the boundary checking is identical to part_nested_loop_var
- * - the return value is shifted toward the left by one unit
+ *Found another variation of partition scheme based on part_nested_loop
+ *Diff wise, it lies between part_nested_loop and part_nested_loop_var
+ *- the order of the two while loops is identical to part_nested_loop
+ *- the boundary checking is identical to part_nested_loop_var
+ *- the return value is shifted toward the left by one unit
  */
 int* part_nested_loop_var2(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
-    while(true) {
-        while(s < e && *++s <= *pivot);  // find the next element >= pivot
-        while(s < e && *pivot <= *--e); // find the prev element <= pivot
+    while (true) {
+        while (s < e && *++s <= *pivot); // find the next element >= pivot
+        while (s < e && *pivot <= *--e); // find the prev element <= pivot
         if (s < e) {
             swap(s, e); //swap the out-of-place elements
         } else {
@@ -87,16 +87,19 @@ int* part_nested_loop_var2(int* s, int* e) {
     }
 }
 
-int* part_thin_loop_var2(int* s, int*e) {
+int* part_thin_loop_var2(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
     ++s;
-    while(true) {
+    while (true) {
         --e;
-        while(s < e && *s <= *pivot)  // find the next element >= pivot
-        {++s;}
-        while(s < e && *pivot <= *e)
-        {--e;}
+        while (s < e && *s <= *pivot) // find the next element >= pivot
+        {
+            ++s;
+        }
+        while (s < e && *pivot <= *e) {
+            --e;
+        }
         if (s < e) {
             swap(s++, e); //swap the out-of-place elements
         } else {
@@ -106,20 +109,19 @@ int* part_thin_loop_var2(int* s, int*e) {
     }
 }
 
-
 /**
- * Variation of partition scheme from part_nested_loop.
- * The two differs in three ways
- * - order of the two while loops
- * - increment condition for equal element pointer
- * - return pointer choice between s and e
+ *Variation of partition scheme from part_nested_loop.
+ *The two differs in three ways
+ *- order of the two while loops
+ *- increment condition for equal element pointer
+ *- return pointer choice between s and e
  */
-int* part_nested_loop_var(int* s, int*e) {
+int* part_nested_loop_var(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
-    while(true) {
-        while(s < e && *pivot <= *--e); // find the prev element <= pivot
-        while(s < e && *++s <= *pivot);  // find the next element >= pivot
+    while (true) {
+        while (s < e && *pivot <= *--e); // find the prev element <= pivot
+        while (s < e && *++s <= *pivot); // find the next element >= pivot
         if (s < e) {
             swap(s, e); //swap the out-of-place elements
         } else {
@@ -134,12 +136,12 @@ Find yet another variation of implementation where the second nested while-loop
 is made post-incremental.
 This will remove the difficulty encountered in loop-thinning.
 */
-int* part_nested_loop_var4(int* s, int*e) {
+int* part_nested_loop_var4(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
-    while(true) {
-        while(s < e && *pivot <= *--e); // find the prev element <= pivot
-        while(s < e && *s <= *pivot)++s;  // find the next element >= pivot
+    while (true) {
+        while (s < e && *pivot <= *--e); // find the prev element <= pivot
+        while (s < e && *s <= *pivot) ++s; // find the next element >= pivot
         if (s < e) {
             swap(s, e); //swap the out-of-place elements
         } else {
@@ -152,13 +154,13 @@ int* part_nested_loop_var4(int* s, int*e) {
 /*
 loop-thinned version of part_nested_loop_var4
 */
-int* part_thin_loop_var4(int* s, int*e) {
+int* part_thin_loop_var4(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s;
     --e;
-    while(true) {
-        if(s < e && *pivot <= *e)--e; // find the prev element <= pivot
-        else if(s < e && *s <= *pivot)++s;  // find the next element >= pivot
+    while (true) {
+        if (s < e && *pivot <= *e) --e; // find the prev element <= pivot
+        else if (s < e && *s <= *pivot) ++s; // find the next element >= pivot
         else if (s < e) {
             swap(s, e--); //swap the out-of-place elements
         } else {
@@ -168,65 +170,65 @@ int* part_thin_loop_var4(int* s, int*e) {
     }
 }
 
-int* part_thin_loop(int* s, int*e) {
+int* part_thin_loop(int* s, int* e) {
     swap(s, s + rand() % (e - s));
     int* const pivot = s++;
     e--;
-    while(true) {
-         if (s < e && *s <= *pivot) {  // find the next element >= pivot
-             ++s;
-         } else if (pivot < e && *pivot <= *e) { // find the prev element <= pivot
-             --e;
-         } else if (s < e) {
-             swap(s++, e--); //swap the out-of-place elements
-         } else {
-             swap(pivot, e);
-             return e;
-         }
+    while (true) {
+        if (s < e && *s <= *pivot) { // find the next element >= pivot
+            ++s;
+        } else if (pivot < e && *pivot <= *e) { // find the prev element <= pivot
+            --e;
+        } else if (s < e) {
+            swap(s++, e--); //swap the out-of-place elements
+        } else {
+            swap(pivot, e);
+            return e;
+        }
     }
 }
 
-int* part_sentinel(int* s, int*e) {
+int* part_sentinel(int* s, int* e) {
     init_swap(s, e);
     int* const pivot = s;
-    while(true) {
-         while(*++s < *pivot);   // find the next element >= pivot
-         while(*--e > *pivot); // find the prev element <= pivot
-         if (s < e) {
-             swap(s, e); //swap the out-of-place elements
-         } else {
-           swap(pivot, e);
-           return e;
-       }
-   }
+    while (true) {
+        while ( *++s < *pivot); // find the next element >= pivot
+        while ( *--e > *pivot); // find the prev element <= pivot
+        if (s < e) {
+            swap(s, e); //swap the out-of-place elements
+        } else {
+            swap(pivot, e);
+            return e;
+        }
+    }
 }
 
-int* part(int* s, int*e) {
+int* part(int* s, int* e) {
     init_swap(s, e);
     int* const pivot = s++;
     --e;
-    while(true) {
-         if (*s < *pivot) {  // find the next element >= pivot
-             ++s;
-         } else if (*e > *pivot) { // find the prev element <= pivot
-             --e;
-         } else if (s < e) {
-             swap(s++, e--); //swap the out-of-place elements
-         } else {
-             swap(pivot, e);
-             return e;
-         }
+    while (true) {
+        if (*s < *pivot) { // find the next element >= pivot
+            ++s;
+        } else if (*e > *pivot) { // find the prev element <= pivot
+            --e;
+        } else if (s < e) {
+            swap(s++, e--); //swap the out-of-place elements
+        } else {
+            swap(pivot, e);
+            return e;
+        }
     }
 }
 
 /**
 This is the Lomuto's partitioning scheme
 */
-int* part1(int* s, int*e) {
+int* part1(int* s, int* e) {
     swap(s, s + rand() % (e - s));
-    int * p = s;
+    int* p = s;
     // printf("Pivot: %d\n", *p);
-    for(int* i = s; i < e; i++ ) {
+    for (int* i = s; i < e; i++) {
         if (*i < *s) {
             swap(++p, i); //swap the out-of-place elements
         }
@@ -238,16 +240,16 @@ int* part1(int* s, int*e) {
 void qSort(int* s, int* e) {
     if (e - s < 2) return;
     if (e - s == 2) {
-     if (*(e - 1) < *s)
-         swap(s, e - 1);
-     return;
+        if (*(e - 1) < *s)
+            swap(s, e - 1);
+        return;
     }
     int* p = part_nested_loop_var4(s, e);
     qSort(s, p);
     qSort(p + 1, e);
 }
 
-const char* msg = "Error: array not sorted properly\n";
+const char *msg = "Error: array not sorted properly\n";
 
 void unit_test(int* test, int size) {
     print(test, size);
@@ -263,19 +265,28 @@ void unit_test_0() {
 }
 
 void unit_test_1() {
-    int test[] = {-1}; //
+    int test[] = {
+        -1
+    }; //
     const int size = 1;
     unit_test(test, size);
 }
 
 void unit_test_2() {
-    int test[] = {-1, -15}; //
+    int test[] = {
+        -1,
+        -15
+    }; //
     const int size = 2;
     unit_test(test, size);
 }
 
 void unit_test_3() {
-    int test[] = {15, -1, -15}; //
+    int test[] = {
+        15,
+        -1,
+        -15
+    }; //
     const int size = 3;
     unit_test(test, size);
 }
@@ -293,7 +304,12 @@ void unit_test_eq() {
 }
 
 void unit_test_4() {
-    int test[] = {-15, 15, -1, -15}; //
+    int test[] = {
+        -15,
+        15,
+        -1,
+        -15
+    }; //
     const int size = 4;
     unit_test(test, size);
 }
@@ -314,7 +330,13 @@ void unit_test_happy() {
 }
 
 void unit_left_bound_exception() {
-    int test[] = {15, -77, 7, -58, 5}; //
+    int test[] = {
+        15,
+        -77,
+        7,
+        -58,
+        5
+    }; //
     const int size = 5;
     unit_test(test, size);
 }
