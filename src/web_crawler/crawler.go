b052924a47a9feb396074bac20ffc06b177e9025
Shoupu Wan <swan@roblox.com>
2022-05-30 13:47:14 -0700

    turn program into multi-thread

---
+++ src/web_crawler/crawler.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+func worker(visited map[string]int, queue *Queue, wg *sync.WaitGroup, limit int) {
+	defer wg.Done()
+	for len(visited) < limit {
+		nextUrl := queue.Pop()
+		if nextUrl == nil {
+			break
+		}
+		fmt.Println(nextUrl)
+		urls := fetch(nextUrl.(string))
+		for _, urlCandidate := range urls {
+			if _, ok := visited[urlCandidate]; ok {
+				continue
+			}
+			visited[urlCandidate] += 1
+			queue.Push(urlCandidate)
+		}
+	}
+}
+
+func valueSum(myDict map[string]int) int {
+	result := 0
+	for _, v := range myDict {
+		result += v
+	}
+	return result
+}
+
+func fetch(pa string) []string {
+	client := &http.Client{}
+	resp, err := client.Get(pa)
+	if err != nil {
+		fmt.Println(err)
+		return []string{}
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+	return extractUrls(resp)
+}
+
+func extractUrls(resp *http.Response) []string {
+	tokenizer := html.NewTokenizer(resp.Body)
+	var hrefMap = make(map[string]int)
+	for {
+		next := tokenizer.Next()
+		if next == html.ErrorToken {
+			err := tokenizer.Err()
+			if err == io.EOF {
+				//end of the file, break out of the loop
+				break
+			}
+			//otherwise, there was an error tokenizing,
+			//which likely means the HTML was malformed.
+			log.Fatalf("error tokenizing HTML: %v", tokenizer.Err())
+		} else if next == html.StartTagToken {
+			name, _ := tokenizer.TagName()
+			if string(name) == "a" {
+				href := extractHref(tokenizer)
+				thisUrl := normalizeUrl(href, resp.Request.URL)
+				log.Println(thisUrl)
+				hrefMap[thisUrl] += 1
+			}
+		}
+	}
+	var result = make([]string, len(hrefMap))
+	i := 0
+	for k := range hrefMap {
+		result[i] = k
+		i++
+	}
+	return result
+}
+
+func normalizeUrl(thisUrl string, req *url.URL) string {
+	parsedUrl, err := url.Parse(thisUrl)
+	if err != nil {
+		return ""
+	}
+	if parsedUrl.Host == "" {
+		parsedUrl.Host = req.Host
+		parsedUrl.Scheme = req.Scheme
+	} else if parsedUrl.Scheme == "" {
+		parsedUrl.Scheme = "http"
+	}
+	if parsedUrl.Path == "" {
+		parsedUrl.Path = "/"
+	}
+	return parsedUrl.String()
+}
+
+func extractHref(tokenizer *html.Tokenizer) string {
+	for {
+		key, val, hasMore := tokenizer.TagAttr()
+		if string(key) == "href" {
+			return string(val)
+		}
+		if !hasMore {
+			break
+		}
+	}
+	return ""
+}
--- src/web_crawler/main.go
+++ src/web_crawler/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/html"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
+	"strconv"
+	"sync"
 )
 
 /**
@@ -19,113 +17,27 @@ It will avoid the cyclic traversal of links.
 
 */
 func main() {
-	pas := os.Args
+	seed, concurrency := os.Args[1], os.Args[2]
+	runJob(concurrency, seed)
+}
+
+func runJob(concurrency string, seed string) {
 	visited := make(map[string]int)
-	queue := NewQueue(1000)
-
-	for _, pa := range pas {
-		queue.Push(pa)
+	concur, err := strconv.Atoi(concurrency)
+	if err != nil {
+		log.Fatalf("error parsing cmd line argument: %v", err)
 	}
+	queue := NewQueue(concur)
+	queue.Push(seed)
 
-	for len(visited) < 1000 && valueSum(visited) < 1000 {
-		pa := queue.Pop()
-		if pa == nil {
-			break
-		}
-		fmt.Println(pa)
-		visited[pa.(string)] += 1
-		if _, ok := visited[pa.(string)]; !ok {
-			continue
-		}
-		urls := fetch(pa.(string))
-		for _, u := range urls {
-			queue.Push(u)
-		}
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go worker(visited, queue, &wg, 1000)
 	}
-
+	wg.Wait()
 	fmt.Println("done")
 	for k, v := range visited {
 		fmt.Println(k, v)
 	}
-}
-
-func valueSum(myDict map[string]int) int {
-	result := 0
-	for _, v := range myDict {
-		result += v
-	}
-	return result
-}
-
-func fetch(pa string) []string {
-	client := &http.Client{}
-	resp, err := client.Get(pa)
-	if err != nil {
-		fmt.Println(err)
-		return []string{}
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
-	return extractUrls(resp)
-}
-
-func extractUrls(resp *http.Response) []string {
-	tokenizer := html.NewTokenizer(resp.Body)
-	var hrefMap = make(map[string]int)
-	for {
-		next := tokenizer.Next()
-		if next == html.ErrorToken {
-			err := tokenizer.Err()
-			if err == io.EOF {
-				//end of the file, break out of the loop
-				break
-			}
-			//otherwise, there was an error tokenizing,
-			//which likely means the HTML was malformed.
-			log.Fatalf("error tokenizing HTML: %v", tokenizer.Err())
-		} else if next == html.StartTagToken {
-			name, _ := tokenizer.TagName()
-			if string(name) == "a" {
-				href := extractHref(tokenizer)
-				thisUrl := normalizeUrl(href, resp.Request.URL)
-				log.Println(thisUrl)
-				hrefMap[thisUrl] += 1
-			}
-		}
-	}
-	var result = make([]string, len(hrefMap))
-	i := 0
-	for k := range hrefMap {
-		result[i] = k
-		i++
-	}
-	return result
-}
-
-func normalizeUrl(thisUrl string, req *url.URL) string {
-	parsedUrl, err := url.Parse(thisUrl)
-	if err != nil {
-		return ""
-	}
-	if parsedUrl.Host == "" {
-		parsedUrl.Host = req.Host
-		parsedUrl.Scheme = req.Scheme
-	} else if parsedUrl.Scheme == "" {
-		parsedUrl.Scheme = "http"
-	}
-	return parsedUrl.String()
-}
-
-func extractHref(tokenizer *html.Tokenizer) string {
-	for {
-		key, val, hasMore := tokenizer.TagAttr()
-		if string(key) == "href" {
-			return string(val)
-		}
-		if !hasMore {
-			break
-		}
-	}
-	return ""
 }
