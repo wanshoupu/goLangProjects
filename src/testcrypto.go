5e2b7d98f18825167fcd5c28bdb9b5e833c8fe41
Shoupu Wan <swan@roblox.com>
2022-08-17 22:05:12 -0700

    add AES encryption scheme (including key gen and key load from PEM file)

--- src/testcrypto.go
+++ src/testcrypto.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"crypto/aes"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
@@ -16,6 +17,7 @@ import (
 
 const privKeyPath = "key/private-key.pem"
 const pubKeyPath = "key/public-key.pem"
+const aesKey = "key/public-key.pem"
 const outputFile = "output/ciphertext"
 
 func main() {
@@ -25,9 +27,96 @@ func main() {
 		keyFile = os.Args[2]
 	} else {
 		//keyFile := privKeyPath
-		keyFile = pubKeyPath
+		//keyFile = pubKeyPath
+		keyFile = aesKey
 	}
 
+	//asymCrypto(keyFile, testMsg)
+	ciphertext := symCrypto(keyFile, testMsg)
+	fmt.Println(ciphertext)
+}
+
+func symCrypto(keyFile string, msg string) string {
+	if _, err := os.Stat(keyFile); errors.Is(err, os.ErrNotExist) {
+		AESKeyGen(keyFile, 32)
+	}
+	key, err := loadAESKey(keyFile)
+	if err != nil {
+		panic("Missing Key")
+	}
+	return EncryptAES(key, msg)
+}
+
+func AESKeyGen(keyFile string, length int) []byte {
+	if stat, _ := os.Stat(keyFile); stat != nil {
+		panic("private key file already exists")
+	}
+
+	keyDir := filepath.Dir(keyFile)
+	if _, err := os.Stat(keyDir); err != nil {
+		os.MkdirAll(keyDir, os.ModePerm)
+	}
+	pemFile, err := os.Create(keyFile)
+	if err != nil {
+		panic(err)
+	}
+
+	key := make([]byte, length)
+
+	// Reads {length} cryptographically secure pseudorandom numbers from rand.Reader and writes them to key
+	_, err = rand.Read(key)
+
+	pemPrivateBlock := &pem.Block{
+		Type:  "AES KEY",
+		Bytes: key,
+	}
+	err = pem.Encode(pemFile, pemPrivateBlock)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	pemFile.Close()
+	return key
+}
+
+func loadAESKey(keyFile string) ([]byte, error) {
+	kf, err := os.Open(keyFile)
+	if err != nil {
+		panic(err)
+	}
+	pemfileinfo, _ := kf.Stat()
+	var size int64 = pemfileinfo.Size()
+	pembytes := make([]byte, size)
+	buffer := bufio.NewReader(kf)
+	_, err = buffer.Read(pembytes)
+	if err != nil {
+		panic(err)
+	}
+	data, _ := pem.Decode(pembytes)
+	kf.Close()
+	if err == nil {
+		return data.Bytes, nil
+	}
+	panic(fmt.Sprintf("loading key failed: \"%s\"", err))
+}
+
+func EncryptAES(key []byte, plaintext string) string {
+	// create cipher
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// allocate space for ciphered data
+	out := make([]byte, len(plaintext))
+
+	// encrypt
+	c.Encrypt(out, []byte(plaintext))
+	// return hex string
+	return base64.StdEncoding.EncodeToString(out)
+}
+
+func asymCrypto(keyFile string, testMsg string) {
 	if _, err := os.Stat(privKeyPath); errors.Is(err, os.ErrNotExist) {
 		keyGen(2048)
 	}
